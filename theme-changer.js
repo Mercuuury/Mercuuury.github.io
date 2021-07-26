909f505c7238ccce7fa49929c42e79974922e763
Mercuuury <kovshiksa@gmail.com>
2021-07-26 14:37:11 +0300

    add support for a dark theme

---
+++ theme-changer.js
@@ -0,0 +1,26 @@
+var btn = document.getElementById("theme-button");
+var link = document.getElementById("theme-link");
+
+btn.addEventListener("click", function () {
+  ChangeTheme();
+});
+
+function ChangeTheme() {
+  let lightTheme = "styles/light.css";
+  let darkTheme = "styles/dark.css";
+
+  var currTheme = link.getAttribute("href");
+  var theme = "";
+
+  if (currTheme == lightTheme) {
+    currTheme = darkTheme;
+    theme = "dark";
+    document.querySelector('img').src = "img/icon-theme-light.svg";
+  } else {
+    currTheme = lightTheme;
+    theme = "light";
+    document.querySelector('img').src = "img/icon-theme-dark.svg";
+  }
+
+  link.setAttribute("href", currTheme);
+}
